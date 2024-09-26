17536d3d19b66b5839a8c6fe4e2ccecc2702ec2a
Lorenzo <139818716+lorenzoponcio@users.noreply.github.com>
2024-09-25 22:25:01 -0300

    Add files via upload

---
+++ Musicas/Musicas/src/main/java/br/edu/univille/poo/tarefas/entity/Musica.java
@@ -0,0 +1,23 @@
+package br.edu.univille.poo.tarefas.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Musica {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+    private String nome;
+    private int duracao;
+
+
+    // Propria Classe / Atributo
+    @ManyToOne
+    private Artista artista;
+    @ManyToOne
+    private Album album;
+}
